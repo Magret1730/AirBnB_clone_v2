ab2682df4fb4458e520811603b8b5317ce75a99a
Magret <belloabiodun17@gmail.com>
2024-03-08 14:08:00 -0800

    Solved last checker issue

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,7 +10,8 @@ exec { 'server setup':
     echo \"<html><head></head><body>Holberton School</body></html>\" | sudo tee /data/web_static/releases/test/index.html > /dev/null &&
     sudo ln -sf /data/web_static/releases/test/ /data/web_static/current &&
     sudo chown -R ubuntu:ubuntu /data/ &&
-    sudo sed -i \'/server_name _;/a\\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}\' /etc/nginx/sites-available/default &&
+    sudo sed -i \'/server_name _;/a\\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}\' \
+      /etc/nginx/sites-available/default &&
     sudo service nginx restart
   '",
   provider => shell,
