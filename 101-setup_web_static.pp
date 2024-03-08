d0341eae506e3a75edc12c24c2a2f3745cca8a07
Magret <belloabiodun17@gmail.com>
2024-03-08 12:15:45 -0800

    Solution to task5 again

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -5,21 +5,33 @@ package { 'nginx':
   ensure => installed,
 }
 
+# Running nginx
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
 # Create necessary directories
 file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
   ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  mode   => '0755',
 }
 
 # Create a fake HTML file
 file { '/data/web_static/releases/test/index.html':
   ensure  => present,
-  content => '<html><head><title>Test Page</title></head><body>Test Content</body></html>',
+  content => '<html><head></head><body>Holberton School</body></html>',
 }
 
 # Create a symbolic link
 file { '/data/web_static/current':
   ensure => link,
   target => '/data/web_static/releases/test',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
 # Set ownership recursively
@@ -27,6 +39,7 @@ file { '/data':
   ensure  => directory,
   owner   => 'ubuntu',
   group   => 'ubuntu',
+  mode    => '0755',
   recurse => true,
 }
 
@@ -39,11 +52,13 @@ file { '/etc/nginx/sites-available/default':
     }
 }",
   require => Package['nginx'],
+  notify  => Exec['nginx-reload'],
 }
 
 # Restart Nginx after updating the configuration
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'nginx-reload':
+  command     => 'sudo service nginx restart',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
 }
