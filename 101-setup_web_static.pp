144af343b967a0654ce1401d5ef613a4625cf685
Magret <belloabiodun17@gmail.com>
2024-03-08 12:37:26 -0800

    Solution to task5 resolved

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,64 +1,6 @@
-# Puppet script to sets up your web servers for the deployment of web_static
+# puppet script setup web server for deployment
 
-# Install Nginx
-package { 'nginx':
-  ensure => installed,
-}
-
-# Running nginx
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
-# Create necessary directories
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0755',
-}
-
-# Create a fake HTML file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => present,
-  content => '<html><head></head><body>Holberton School</body></html>',
-}
-
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-# Set ownership recursively
-file { '/data':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0755',
-  recurse => true,
-}
-
-# Update Nginx configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Exec['nginx-reload'],
-}
-
-# Restart Nginx after updating the configuration
-exec { 'nginx-reload':
-  command     => 'sudo service nginx restart',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
+exec { 'server setup':
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
+  provider => shell,
 }
