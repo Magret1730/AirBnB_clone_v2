656f5b01e5669256ad7277317869eec0d44b20b1
Magret <belloabiodun17@gmail.com>
2024-03-08 14:01:30 -0800

    Solved last checker issue

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,7 +7,7 @@ exec { 'server setup':
     sudo service nginx start &&
     sudo mkdir -p /data/web_static/shared/ &&
     sudo mkdir -p /data/web_static/releases/test/ &&
-    echo \"Holberton School\" | sudo tee /data/web_static/releases/test/index.html > /dev/null &&
+    echo \"<html><head></head><body>Holberton School</body></html>\" | sudo tee /data/web_static/releases/test/index.html > /dev/null &&
     sudo ln -sf /data/web_static/releases/test/ /data/web_static/current &&
     sudo chown -R ubuntu:ubuntu /data/ &&
     sudo sed -i \'/server_name _;/a\\tlocation /hbnb_static {\\n\\t\\talias /data/web_static/current/;\\n\\t}\' /etc/nginx/sites-available/default &&
@@ -15,5 +15,3 @@ exec { 'server setup':
   '",
   provider => shell,
 }
-
-# command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo service nginx start && sudo mkdir -p /data/web_static/shared/ && sudo mkdir -p /data/web_static/releases/test/ && echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sudo sed -i \'44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\' /etc/nginx/sites-available/default && sudo service nginx restart',
