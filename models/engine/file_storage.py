dd3195d88647af34642a26cacdde4210e95a1cad
Magret <belloabiodun17@gmail.com>
2024-03-22 12:02:16 +0000

    update file starage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,3 +59,7 @@ class FileStorage:
             # print(key)
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
